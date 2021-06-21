c10055a78074d8efd36838c20c295a3c4d35db48
Oscar De Leon Gomez <oleongomez@gmail.com>
2021-06-21 00:38:55 -0700

    added prices to stock object

--- stock.js
+++ stock.js
@@ -1,8 +1,8 @@
-const data=[{"name":"apple", "quantity":1},
-    {"name":"pinapple", "quantity":10},
-    {"name":"mango", "quantity":25},
-    {"name":"pear", "quantity":2},
-    {"name":"orange", "quantity":30},
-    {"name":"guava", "quantity":12},
-    {"name":"avocado", "quantity":12}
+const data=[{"name":"apple", "quantity":1, "price":2.0},
+    {"name":"pinapple", "quantity":10, "price":2.0},
+    {"name":"mango", "quantity":25, "price":2.0},
+    {"name":"pear", "quantity":2, "price":2.0},
+    {"name":"orange", "quantity":30, "price":2.0},
+    {"name":"guava", "quantity":12, "price":2.0},
+    {"name":"avocado", "quantity":12, "price":2.0}
 ]
